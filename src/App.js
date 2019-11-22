9f4798e8088316248263900742fc174122ff04e8
Yara Castro <yaracastro24@gmail.com>
2019-11-21 18:50:00 -0600

    MVP - Completed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-
+import TodoList from './components/TodoList';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-
+      <TodoList/>
     </div>
   );
 }
---
+++ src/components/TodoList.js
@@ -0,0 +1,20 @@
+import React, { useReducer } from 'react';
+
+import Todo from './Todos';
+import TodoForm from './TodoForm';
+import { todoReducer, initialTodosState } from './reducers/reducer';
+
+export default function TodoList() {
+    const [ state, dispatch ] = useReducer(todoReducer, initialTodosState);
+
+    return (
+        <div>
+            {
+                state.map( (todo) => {
+                return <Todo todo={todo} key={todo.id} dispatch={dispatch} />
+                })
+            }
+        <TodoForm dispatch={dispatch} />
+        </div>
+    );
+}
